4e38c6f7db510fcab7bdc82390d171c5c1e61ebe
Dennis Stücken <dstuecken@me.com>
2017-11-07 01:37:38 +0100

    create log files if not existent

--- app/bootstrap/Services/Logger.php
+++ app/bootstrap/Services/Logger.php
@@ -1,4 +1,5 @@
 <?php
+
 use Phalcon\Logger\Adapter\File as FileAdapter;
 use Phalcon\Logger\Multiple as MultipleStream;
 
@@ -11,36 +12,46 @@ use Phalcon\Logger\Multiple as MultipleStream;
  * @version $Version$
  */
 
-return function (\DS\Interfaces\GeneralApplication $application, Phalcon\Di\FactoryDefault $di)
-{
+return function (\DS\Interfaces\GeneralApplication $application, Phalcon\Di\FactoryDefault $di) {
     $di->set(
         \DS\Constants\Services::ERRORLOGGER,
-        function ()
-        {
+        function () {
             /*
             $logger = new MultipleStream();
             $logger->push(new FileAdapter(ROOT_PATH . 'system/log/error'));
             */
-
+            if (!file_exists(ROOT_PATH . 'system/log/error'))
+            {
+                touch(ROOT_PATH . 'system/log/error');
+            }
+            
             return new FileAdapter(ROOT_PATH . 'system/log/error');
         }
     );
-
+    
     $di->set(
         \DS\Constants\Services::LOGGER,
-        function ()
-        {
+        function () {
+            if (!file_exists(ROOT_PATH . 'system/log/application'))
+            {
+                touch(ROOT_PATH . 'system/log/application');
+            }
+            
             return new FileAdapter(ROOT_PATH . 'system/log/application');
         }
     );
-
+    
     $di->set(
         \DS\Constants\Services::CLILOGGER,
-        function ()
-        {
+        function () {
+            if (!file_exists(ROOT_PATH . 'system/log/cli'))
+            {
+                touch(ROOT_PATH . 'system/log/cli');
+            }
+            
             $logger = new MultipleStream();
             $logger->push(new FileAdapter(ROOT_PATH . 'system/log/cli'));
-
+            
             return $logger;
         }
     );
