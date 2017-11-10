50c4262f189dada342476adf0f92530b94f70a16
Dennis Stücken <dstuecken@me.com>
2017-11-10 14:13:49 +0100

    apidocs updated

--- app/controllers/Api/v1/Locations/Get.php
+++ app/controllers/Api/v1/Locations/Get.php
@@ -33,12 +33,14 @@ class Get extends ActionHandler implements MethodInterface
     /**
      * Process Get Method
      *
-     * @api               {get} /locations Request all locations
+     * @api               {get} /api/v1/locations Request all locations
      * @apiVersion        1.0.0
      * @apiName           Locations
      * @apiGroup          Public
      *
-     * @apiSuccess {Object} _meta Meta object with properties total and success
+     * @apiSuccess {Object} _meta Meta object
+     * @apiSuccess {int} _meta.total total number of items included in this response
+     * @apiSuccess {bool} _meta.success Defines whether the request had any errors
      * @apiSuccess {Object[]} data Array of locations
      * @apiSuccess {string} data.id Id of location
      * @apiSuccess {string} data.locationName Name of location
@@ -46,24 +48,24 @@ class Get extends ActionHandler implements MethodInterface
      * @apiSuccessExample Success-Response:
      *     HTTP/1.1 200 OK
      *     {
-     *      "_meta": {
-     *              "total":3,
-     *              "success":true
+     *          "_meta": {
+     *              "total": 3,
+     *              "success": true
      *          },
-     *      "data": [
-     *          {
-     *              "id":"1",
-     *              "locationName":"Berlin",
-     *          },
-     *          {
-     *              "id":"2",
-     *              "locationName":"Germany",
-     *          },
-     *          {
-     *              "id":"3",
-     *              "locationName":"Cologne",
-     *          }
-     *       ]
+     *          "data": [
+     *              {
+     *                  "id": "1",
+     *                  "locationName": "Berlin",
+     *              },
+     *              {
+     *                  "id": "2",
+     *                  "locationName": "Germany",
+     *              },
+     *              {
+     *                  "id": "3",
+     *                  "locationName": "Cologne",
+     *              }
+     *          ]
      *      }
      *
      * @return mixed
