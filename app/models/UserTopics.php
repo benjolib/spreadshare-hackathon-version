73ae0f13730ca18e11fc2819e6958a9020df4730
Dennis Stücken <dstuecken@me.com>
2017-11-15 15:40:34 +0100

    UserTopics Models added

---
+++ app/models/UserTopics.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DS\Model;
+
+use DS\Model\Events\UserTopicsEvent;
+
+/**
+ * UserSettings
+ *
+ * @author    Dennis Stücken
+ * @license   proprietary
+ * @copyright Spreadshare
+ * @link      https://www.spreadshare.co
+ *
+ * @version   $Version$
+ * @package   DS\Model
+ *
+ * @method static findFirstById(int $id)
+ */
+class UserTopics
+    extends UserTopicsEvent
+{
+}
