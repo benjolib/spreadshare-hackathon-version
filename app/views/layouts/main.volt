6288650005cccbb4e0c6d2deff3be55b68478c0c
Dennis Stücken <dstuecken@me.com>
2017-11-15 19:11:52 +0100

    created react build is now auto-replaced within layouts/main.volt

--- app/views/layouts/main.volt
+++ app/views/layouts/main.volt
@@ -104,7 +104,7 @@
     </div>
   </footer>
 </section>
-<script type="text/javascript" src="/js/react/main.d1c7c06e.js"></script>
+<script type="text/javascript" src="/js/react/main.f8c5d60c.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.slim.min.js"></script>
 </body>
 </html>
--- react/build.js
+++ react/build.js
@@ -1,5 +1,27 @@
 const copy = require('recursive-copy');
 
+const fs = require('fs');
+const path = require('path');
+
+// vars
+const mainLayoutFile = '../app/views/layouts/main.volt';
+const directory = '../public/js/react';
+
+// Removing previous build
+fs.readdir(directory, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    if (file !== '.gitkeep') {
+      fs.unlink(path.join(directory, file), err => {
+        if (err) throw err;
+      });
+    }
+  }
+});
+
+
+// Copying build into public js directory
 const options = {
   overwrite: true,
   expand: true,
@@ -9,6 +31,26 @@ const options = {
 copy('./build/static/js', '../public/js/react', options)
   .then(function (results) {
     console.info('Copied ' + results.length + ' files');
+
+    // Replacing main bundle reference in backend's layout file
+    results.forEach((result) => {
+      const found = result.dest.match(/react\/(main\..*?\.js)$/i);
+      if (found) {
+        const buildFile = found[1];
+
+        fs.readFile(mainLayoutFile, 'utf8', function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+        const result = data.replace(/main\..*?\.js/g, buildFile);
+
+        fs.writeFile(mainLayoutFile, result, 'utf8', function (err) {
+           if (err) return console.log(err);
+        });
+      });
+      }
+
+    })
   })
   .catch(function (error) {
     console.error('Copy failed: ' + error);
