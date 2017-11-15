e8170886617f86fa9235ee6b1aa8e3658f0ce915
Dennis Stücken <dstuecken@me.com>
2017-11-15 17:43:21 +0100

    Table container is now uppercase

---
+++ react/src/containers/Table_/actions.js
@@ -0,0 +1,14 @@
+export const TABLE_GET = 'table/GET';
+export const TABLE_RECEIVED = 'table/RECEIVED';
+export const TABLE_RECEIVE_FAILED = 'table/RECEIVED';
+
+export const tableReceived = (data) => {
+  return dispatch => {
+    dispatch({
+      type: TABLE_RECEIVED,
+      payload: {
+        data,
+      },
+    });
+  };
+};
