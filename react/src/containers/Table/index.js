2b7cf6abd07c6bbbf98cd76d4c1ce5840e335a12
Stanley E. M. Day <stan@standayweb.com>
2017-11-17 17:36:40 +0000

    progress on table styling & working out handsontable

--- react/src/containers/Table/index.js
+++ react/src/containers/Table/index.js
@@ -6,13 +6,57 @@ import HotTable from 'react-handsontable';
 class Table extends React.Component {
   render() {
     console.log(this.props);
-    return <div>
-      <HotTable
-        data={this.props.data}
-        contextMenu={true}
-        colHeaders={true}
-      />
-    </div>;
+    // TODO: split into some child components
+    return (
+      <div className="Table">
+        <header className="Table__header">
+          <button className="Table__header__button"><img src="/assets/icons/sort.svg" /></button>
+          <button className="Table__header__button"><img src="/assets/icons/filter.svg" /></button>
+          <button className="Table__header__button"><img src="/assets/icons/add.svg" /></button>
+          <div className="Table__header__search">
+            <button><img src="/assets/icons/search.svg" /></button>
+            <input type="text" placeholder="Search Table" />
+          </div>
+          <button className="Table__header__button"><img src="/assets/icons/eye.svg" /></button>
+        </header>
+        <main className="Table__main">
+          {/* Votes may not need to be seperate after all */}
+          {/* <div className="Table__main__votes">Votes</div> */}
+          <HotTable
+            className="Table__main__HotTable"
+            data={this.props.data}
+            contextMenu={true}
+            colHeaders={['Votes', 'Name', 'Laptop Pocket', 'Luggage Attach', 'Shoulder Padding', 'Front Loading', 'ClamShell', 'Weight Bearing Hip Belt', 'Rain Cover', 'Fits Lowcost Carry-on', 'Dimensions (in)', 'Max Volume (L)', 'Max Range (L)', 'Weight (lbs)', 'Vol. / Lb.', 'Price ($)', 'Price / Vol. / Lb.', 'URL of offical website', 'Review']}
+            columns={[
+              {
+                width: 100,
+                sortIndicator:  true
+              },
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {}
+            ]}
+            fixedColumnsLeft={1}
+            stretchH="all"
+          />
+        </main>
+      </div>
+    );
   }
 }
 
--- react/src/containers/Table/reducer.js
+++ react/src/containers/Table/reducer.js
@@ -1,15 +1,16 @@
 import { TABLE_RECEIVED } from './actions';
 
+// TODO: flow typing
+// TODO: tests
+
 const initialState = {
   data: [
-    [
-      'some',
-      'test',
-    ],
-    [
-      'handsontable',
-      'data',
-    ],
+    // TODO: real data
+    [300, 'Timbuk2 Aviator Travel Backpack', '✓', '', '', '✓', '', 'Average', '✓', '', '22 x 12.5 x 9', '28', '', '4.4', '=K4/M4', '$178', '=O4/N4', '', ''],
+    [250, 'Deuter ACT Trail 30 backpack', '✓',	'✗', '✓', '✓', '✗', '✓', '✓', '24.4 x 11.5 x 8.3', '30', '2.65', '11.32', '$129', '$11'],
+    [100, '', -11, 14, 13],
+    [50, '', 15, -12, 'readOnly'],
+    [50, '', 15, -12, 'readOnly']
   ],
 };
 
