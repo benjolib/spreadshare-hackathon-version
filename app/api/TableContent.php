ef3dd3850ed0450862d3ec84c0900f4b0a91b1cf
Dennis Stücken <dstuecken@me.com>
2017-11-21 00:31:39 +0100

    internal api TableContent added

---
+++ app/api/TableContent.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace DS\Api;
+
+use DS\Component\Text\Csv;
+use DS\Model\TableCells;
+use DS\Model\TableColumns;
+use DS\Model\TableRows;
+use DS\Model\Tables;
+use Phalcon\Exception;
+
+/**
+ * Spreadshare
+ *
+ * Table Content api
+ *
+ * @author    Dennis Stücken
+ * @license   proprietary
+ * @copyright Spreadshare
+ * @link      https://www.spreadshare.co
+ *
+ * @version   $Version$
+ * @package   DS\Model
+ *
+ * @method static findFirstById(int $id)
+ */
+class TableContent
+    extends BaseApi
+{
+    /**
+     * Get table rows and votes
+     *
+     * @param int $tableId
+     *
+     * @return array
+     */
+    public function getTableRows(int $tableId)
+    {
+        $tableRows = new TableRows();
+        $rows      = $tableRows->getRowsForTable($tableId);
+        
+        $votesData = $rowData = [];
+        foreach ($rows as $row)
+        {
+            $votesData[] = $row->votesCount;
+            $rowData[]   = $row->content;
+        }
+        
+        return [
+            'data' => Tables::get($tableId)->toArray(['title']),
+            'votes' => $votesData,
+            'rows' => $rowData,
+        ];
+    }
+    
+    /**
+     * @param int    $tableId
+     * @param string $csvData
+     * @param string $separator
+     */
+    public function addfromCsvText(int $tableId, string $csvData = '', $separator = ',', $hasHeaders = false)
+    {
+        $userId = serviceManager()->getAuth()->getUserId();
+        
+        $csv  = new Csv();
+        $rows = $csv->parseFromText($csvData, $separator, true, true);
+        
+        if (!is_array($rows) || !isset($rows[0]))
+        {
+            throw new \InvalidArgumentException('Unable to parse csv file.');
+        }
+        
+        // Check for semicolon separated text
+        if (count($rows[0]) === 1 && substr_count($rows[0][0], ';') > 0)
+        {
+            return $this->addfromCsvText($tableId, $csvData, ';', $hasHeaders);
+        }
+        
+        // Start sql transaction
+        $db = $this->serviceManager->getDI()->get('db');
+        $db->begin();
+        
+        try
+        {
+            $columnIds = [];
+            $i         = 1;
+            foreach ($rows[0] as $key => $headerField)
+            {
+                $columnModel = new TableColumns();
+                $columnModel->setTableId($tableId)
+                            ->setPosition($i)
+                            ->setUserId($userId)
+                            ->setWidth(100)
+                            ->setTitle($hasHeaders ? $headerField : 'Column ' . $i++)
+                            ->create();
+                
+                $columnIds[$key] = $columnModel->getId();
+            }
+            
+            if ($hasHeaders)
+            {
+                unset($rows[0]);
+            }
+            
+            if (count($columnIds))
+            {
+                $line = 1;
+                foreach ($rows as $row)
+                {
+                    if (is_array($row))
+                    {
+                        $rowModel = new TableRows();
+                        $rowModel->setUserId($userId)
+                                 ->setTableId($tableId)
+                                 ->setLineNumber($line++)
+                                 ->setCommentsCount(0)
+                                 ->setVotesCount(0)
+                                 ->setContent(json_encode($row))
+                                 ->create();
+                        
+                        foreach ($row as $key => $cell)
+                        {
+                            $cellModel = new TableCells();
+                            $cellModel->setUserId($userId)
+                                      ->setRowId($rowModel->getId())
+                                      ->setColumnId($columnIds[$key])
+                                      ->setContent($cell)
+                                      ->setLink('')// @todo may parse link and add it here
+                                      ->create();
+                            
+                        }
+                    }
+                }
+            }
+            
+            $db->commit();
+        }
+        catch (Exception $e)
+        {
+            $db->rollback();
+            throw $e;
+        }
+    }
+}
