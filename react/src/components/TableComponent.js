431379784e7339ed251e16071efb66e03843aa31
Enrique Benitez <enriquebenitezjr@gmail.com>
2017-11-19 20:05:55 -0600

    Style Handsontable

---
+++ react/src/components/TableComponent.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import HotTable from 'react-handsontable';
+import styled from 'styled-components';
+
+export default ({ data }) => {
+  return (
+    <div className="table">
+      <header className="table__header">
+        <button className="table__header__button"><img src="/assets/icons/sort.svg" /></button>
+        <button className="table__header__button"><img src="/assets/icons/filter.svg" /></button>
+        <button className="table__header__button"><img src="/assets/icons/add.svg" /></button>
+        <div className="table__header__search">
+          <button><img src="/assets/icons/search.svg" /></button>
+          <input type="text" placeholder="Search Table" />
+        </div>
+        <button className="table__header__button"><img src="/assets/icons/eye.svg" /></button>
+      </header>
+      <main className="table__main">
+        <StyledTable
+          className="table__main__hotTable"
+          data={data}
+          contextMenu={true}
+          colHeaders={['Votes', 'Name', 'Functionality', 'URL', 'Price']}
+          stretchH="all"
+          columns={ [{},{},{},{},{}] }
+        />
+      </main>
+    </div>
+  );
+}
+
+const StyledTable = styled(HotTable)`
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+  color: #6A7C94;
+
+  &.area.highlight {
+    background: rgba(36, 174, 105, 0.3);
+  }
+
+  .htBorders {
+    .wtBorder.fill {
+      background-color: #24AE69 !important;
+    }
+  }
+
+  .wtBorder.current, .wtBorder.area, .wtBorder.area.corner {
+    background-color: rgb(36, 174, 105) !important;
+  }
+
+  .handsontableInput {
+    color: #6A7C94;
+    box-shadow: 0 0 0 2px #24AE69 inset;
+  }
+
+  td {
+    border: none;
+    padding: 10px;
+  }
+`;
--- react/src/containers/Table/reducer.js
+++ react/src/containers/Table/reducer.js
@@ -5,12 +5,23 @@ import { TABLE_RECEIVED } from './actions';
 
 const initialState = {
   data: [
-    // TODO: real data
-    [300, 'Timbuk2 Aviator Travel Backpack', '✓', '', '', '✓', '', 'Average', '✓', '', '22 x 12.5 x 9', '28', '', '4.4', '=K4/M4', '$178', '=O4/N4', '', ''],
-    [250, 'Deuter ACT Trail 30 backpack', '✓',	'✗', '✓', '✓', '✗', '✓', '✓', '24.4 x 11.5 x 8.3', '30', '2.65', '11.32', '$129', '$11'],
-    [100, '', -11, 14, 13],
-    [50, '', 15, -12, 'readOnly'],
-    [50, '', 15, -12, 'readOnly']
+    [2000, 'Talkus', 'Centralize all your customer feedback (livechat, email, sms, twitter, facebook) in one place, Slack', 'http://talkus.io', '$48/mo unlimited agents'],
+    [1998, 'UserTest.io', 'Video feedback of your users voicing their thoughts and opions as they complete tasks.', 'https://usertest.io', '$10 per Your Own Tester, $23 per Qualified Website Tester'],
+    [1990, 'UserTesting', 'Video feedback on your website or app', 'https://www.usertesting.com', '1 free 5min review, $49 per review thereafter'],
+    [1988, 'Temper', 'Temper measures how your customers feel about your business so you know what to improve', 'https://www.temper.io/', '$12/mo.'],
+    [1802, 'LiveChat', 'Premium live chat and help desk software for websites and apps', 'https://www.livechatinc.com', '$16/mo'],
+    [1791, 'Olark', 'Experience the easiest way to boost your sales, help solve issues and understand your customers.', 'https://www.olark.com/', '15$/mo.'],
+    [1700, 'Intercom', 'Intercom is one place for your entire business to see and talk to customers.', 'https://www.intercom.io/', 'https://www.intercom.io/pricing'],
+    [1697, 'Qualaroo', 'Get Voice of Customer Insights that help Your Business Grow.', 'https://qualaroo.com/', '63$/mo.'],
+    [1696, 'Ottspott', 'Phone system for Slack marketing, sales & support teams.', 'http://ottspott.co/', 'Startup Pack: €49/month including 3 users'],
+    [1690, 'UsabilityTools', 'Optimize websites for better user experience and higher conversion', 'http://usabilitytools.com/', 'Differs'],
+    [1450, 'Typeform', 'Typeform specializes in beautiful, intuitive survey experiences', 'https://www.typeform.com/', 'Free'],
+    [1349, 'Tawk', 'Totally free live chat :)', 'https://tawk.to', 'Free'],
+    [1298, 'Trustbadge', 'Grow your Business & Show that your customers love you', 'http://www.trustbadge.com/', 'Free'],
+    [1100, 'Hotline', 'In app support chat, user engagement and FAQs for Mobile apps', 'https://www.hotline.io', 'Free Upto 25k MAU'],
+    [1000, 'Crisp', 'Simple and affordable live chat for your website.', 'https://crisp.chat/', 'Free, $25/m, $95/m'],
+    [996, 'Vero', 'Automated emails based on user behaviour', 'http://www.getvero.com/', 'From $99/m'],
+    [878, 'elevio', 'Display support content in-app, and get implicit and explicit feedback on the content, in context', 'https://elev.io', 'Starting at $49/m']
   ],
 };
 
