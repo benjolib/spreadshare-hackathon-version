307a664dd8c65e2057d48e391631b68a1a988178
Dennis Stücken <dstuecken@me.com>
2017-11-26 16:09:46 +0100

    EditCell Api method implemented, EditRow Api method updated

---
+++ app/controllers/Api/v1/EditCell/Post.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace DS\Controller\Api\v1\EditCell;
+
+use DS\Api\TableContent;
+use DS\Controller\Api\ActionHandler;
+use DS\Controller\Api\Meta\Record;
+use DS\Controller\Api\MethodInterface;
+use DS\Model\ChangeRequests;
+use DS\Model\TableCells;
+use DS\Model\Tables;
+
+/**
+ *
+ * Spreadshare
+ *
+ * @author    Dennis Stücken
+ * @license   proprietary
+ * @copyright Spreadshare
+ * @link      https://www.spreadshare.co
+ *
+ * @version   $Version$
+ * @package   DS\Controller
+ */
+class Post extends ActionHandler implements MethodInterface
+{
+    /**
+     * @return bool
+     */
+    public function needsLogin()
+    {
+        return true;
+    }
+    
+    /**
+     * Process Post Method
+     *
+     * @api               {post} /api/v1/edit-cell/:cellId Post an edited cell (:cellId is the id of the cell)
+     * @apiParam {String} [content]  Cell content
+     * @apiParam {String} [link]  Cell link / null if there is none
+     * @apiVersion        1.0.0
+     * @apiName           Edit Cell
+     * @apiGroup          Public
+     *
+     * @apiSuccess {Object} _meta Meta object
+     * @apiSuccess {int} _meta.total total number of items included in this response
+     * @apiSuccess {bool} _meta.success Defines whether the request had any errors
+     * @apiSuccess {bool} result
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *          "_meta": {
+     *              "total": 1,
+     *              "success": true
+     *          },
+     *          "data": true
+     *      }
+     *
+     * @return mixed
+     */
+    public function process()
+    {
+        if ($this->request->isAjax())
+        {
+            $cellId  = $this->action;
+            $comment = $this->request->getPost('comment');
+            $content = $this->request->getPost('content');
+            $link    = $this->request->getPost('link');
+            
+            if ($cellId > 0 && $content)
+            {
+                $tableCell = TableCells::findFirstById($cellId);
+                
+                if (!$tableCell)
+                {
+                    throw new \InvalidArgumentException('The cell that you want to edit does not exist.');
+                }
+                
+                $tableId = $tableCell->getTableColumns()->getTableId();
+                
+                $tableModel = Tables::findFirstById($tableId);
+                if (!$tableModel)
+                {
+                    throw new \InvalidArgumentException('The table that you want to edit does not exist.');
+                }
+                
+                // User is Owner and can directly edit!
+                if ($tableModel->getOwnerUserId() == $this->getServiceManager()->getAuth()->getUserId())
+                {
+                    $tableContent = new TableContent();
+                    $tableContent->editCell($tableId, $content, $link ? $link : null);
+                }
+                // User contribution has to be confirmed first.
+                else
+                {
+                    $changeRequest = new ChangeRequests();
+                    $changeRequest->setCellId($cellId)
+                                  ->setFrom($tableCell->getContent())
+                                  ->setTo($content)
+                                  ->setComment($comment)
+                                  ->create();
+                }
+                
+                return new Record(true);
+            }
+        }
+        
+        return new Record(false);
+    }
+    
+}
--- app/controllers/Api/v1/EditRow/Post.php
+++ app/controllers/Api/v1/EditRow/Post.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace DS\Controller\Api\v1\Table;
+namespace DS\Controller\Api\v1\EditRow;
 
 use DS\Api\TableContent;
 use DS\Controller\Api\ActionHandler;
 use DS\Controller\Api\Meta\Record;
 use DS\Controller\Api\MethodInterface;
+use DS\Model\Tables;
 
 /**
  *
@@ -32,11 +33,11 @@ class Post extends ActionHandler implements MethodInterface
     /**
      * Process Post Method
      *
-     * @api               {post} /api/v1/table/:tableId Post an edited row (:tableId is the id of the table)
+     * @api               {post} /api/v1/edit-row/:tableId Post an edited row (:tableId is the id of the table)
      * @apiParam {String} [rowData]  The row as a json string
      * @apiParam {String} [lineNumber]  Row Index / Line Number
      * @apiVersion        1.0.0
-     * @apiName           Table
+     * @apiName           Edit Row
      * @apiGroup          Public
      *
      * @apiSuccess {Object} _meta Meta object
@@ -66,8 +67,23 @@ class Post extends ActionHandler implements MethodInterface
             
             if ($tableId > 0 && $lineNumber > 0 && $rowData)
             {
-                $tableContent = new TableContent();
-                $tableContent->editRow($tableId, $lineNumber, $rowData);
+                $tableModel = Tables::findFirstById($tableId);
+                if (!$tableModel)
+                {
+                    throw new \InvalidArgumentException('The table that you want to edit does not exist.');
+                }
+                
+                // User is Owner and can directly edit!
+                if ($tableModel->getOwnerUserId() == $this->getServiceManager()->getAuth()->getUserId())
+                {
+                    $tableContent = new TableContent();
+                    $tableContent->editRow($tableId, $lineNumber, $rowData);
+                }
+                // User contribution has to be confirmed first.
+                else
+                {
+                
+                }
                 
                 return new Record(true);
             }
