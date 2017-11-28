83e3daf80ca3493258187556d9bd42f022f5d073
Stanley Day <stan@standayweb.com>
2017-11-28 16:40:13 +0000

    API_URL: spreadshare.docker -> dev.spreadshare.co

--- react/src/api.js
+++ react/src/api.js
@@ -1,7 +1,7 @@
 // @flow
 import fetchWrapper from "./lib/fetchWrapper";
 
-const API_URL = "http://spreadshare.docker:81";
+const API_URL = "http://dev.spreadshare.co:81";
 
 export const fetchDataApi = (method: string) =>
   fetchWrapper(`${API_URL}/api/v1/${method}`, {
