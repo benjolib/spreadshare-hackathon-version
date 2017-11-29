c59b658878e7156cef065a24205c5860f9e3aa7c
Dennis Stücken <dstuecken@me.com>
2017-11-29 12:44:49 +0100

    two find model traits added

---
+++ app/traits/Model/FindUserAndRowTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DS\Traits\Model;
+
+/**
+ * Spreadshare
+ *
+ * @author    Dennis Stücken
+ * @license   proprietary
+ * @copyright Spreadshare
+ * @link      https://www.spreadshare.co
+ *
+ * @version   $Version$
+ * @package   DS\Interfaces
+ */
+trait FindUserAndRowTrait
+{
+    /**
+     * @param int $userId
+     * @param int $rowId
+     *
+     * @return self
+     */
+    public static function findByUserIdAndRow(int $userId, int $rowId): self
+    {
+        return self::findFirst(
+            [
+                "conditions" => 'userId = ?0 AND rowId = ?1',
+                "bind" => [$userId, $rowId],
+                "limit" => 1,
+            ]
+        );
+    }
+}
---
+++ app/traits/Model/FindUserAndTableTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DS\Traits\Model;
+
+/**
+ * Spreadshare
+ *
+ * @author    Dennis Stücken
+ * @license   proprietary
+ * @copyright Spreadshare
+ * @link      https://www.spreadshare.co
+ *
+ * @version   $Version$
+ * @package   DS\Interfaces
+ */
+trait FindUserAndTableTrait
+{
+    /**
+     * @param int $userId
+     * @param int $tableId
+     *
+     * @return self
+     */
+    public static function findByUserIdAndTable(int $userId, int $tableId): self
+    {
+        return self::findFirst(
+            [
+                "conditions" => 'userId = ?0 AND tableId = ?1',
+                "bind" => [$userId, $tableId],
+                "limit" => 1,
+            ]
+        );
+    }
+}
