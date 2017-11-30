919e72aaa82b7ae4c69505782b548fa13ad09651
Rohan Dey <me@rohandey.com>
2017-12-01 04:24:40 +0530

    New About Page

---
+++ app/listeners/Wallet.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace DS\Listeners;
+
+use DS\Application;
+use Elastica\Document;
+use Elastica\Exception\ResponseException;
+use Elastica\Index;
+use Elastica\Response;
+use Bernard\Message\PlainMessage;
+use DS\Component\ServiceManager;
+
+
+class ElasticSearch
+{
+
+
+    public function newTable(PlainMessage $message)
+    {
+
+      try {
+
+        // Create a table
+        $tableData = [
+          'id' => $message->get('tableId'),
+          'title' => $message->get('tableTitle'),
+          'tagline' => $message->get('tableTagline')
+        ];
+
+        // getClient
+        $elasticaClient = self::getClient();
+        // Load index
+        $elasticaIndex = $elasticaClient->getIndex('tables');
+
+        if (!$elasticaIndex->exists()) {
+          # Create index
+          $elasticaIndex->create();
+        }
+
+        // Get type
+        $elasticaType = $elasticaIndex->getType('table');
+
+        // Document addition
+        $tableDocument = new \Elastica\Document($message->get('tableId'), $tableData);
+
+        // Add table to type
+        $elasticaType->addDocument($tableDocument);
+
+
+
+      } catch (ResponseException $e) {
+
+        $error = $e->getResponse()->getFullError();
+
+        Application::instance()->log($error['type'].':'.$error['reason'], Logger::CRITICAL);
+
+      }
+
+    }
+
+
+}
