3e4ff5722e4c1cffe216aa120b08752aed05495d
Stanley E. M. Day <stan@standayweb.com>
2017-11-30 19:07:15 +0000

    loading height

--- react/src/components/TableLoading.js
+++ react/src/components/TableLoading.js
@@ -1,6 +1,6 @@
 // @flow
 import React from "react";
 
-const TableLoading = () => <div className="loading" />;
+const TableLoading = () => <div className="loading" style={{ height: 100 }} />;
 
 export default TableLoading;
