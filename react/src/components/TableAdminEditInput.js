c16d0d54bb584a0e6a2bc42ad5efcce38e3af79b
Stanley E. M. Day <stan@standayweb.com>
2017-11-30 18:53:39 +0000

    various improvements/fixes

--- react/src/components/TableAdminEditInput.js
+++ react/src/components/TableAdminEditInput.js
@@ -80,7 +80,7 @@ class TableAdminEditInput extends React.Component<Props> {
     return (
       <StyledDiv>
         <button>
-          <img src="/assets/icons/search.svg" alt="" />
+          <img src="/assets/icons/pencil.svg" alt="" />
         </button>
         <input
           type="text"
--- react/src/lib/cellRenderer.js
+++ react/src/lib/cellRenderer.js
@@ -1,9 +1,17 @@
 // @flow
+import escapeHtml from "./escapeHtml";
+
 const cellRenderer = data => (instance, td, row, col, prop, value) => {
   if (value && value.link && value.content) {
-    td.innerHTML = `<div style="padding: 8px 0;"><a href="${value.link}" target="_blank" style="color:#62b38c;">${value.content}</a></div>`;
+    td.innerHTML = `<div style="padding: 8px 0;"><a href="${escapeHtml(
+      value.link
+    )}" target="_blank" style="color:#62b38c;text-decoration: underline;">${escapeHtml(
+      value.content
+    )}</a></div>`;
   } else if (value && value.content) {
-    td.innerHTML = `<div style="padding: 8px 0;">${value.content}</div>`;
+    td.innerHTML = `<div style="padding: 8px 0;">${escapeHtml(
+      value.content
+    )}</div>`;
   } else {
     td.innerHTML = `<div style="padding: 8px 0;">&nbsp;</div>`;
   }
