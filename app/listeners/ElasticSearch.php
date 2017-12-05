4536459c263a078e3c97b08c29441c83710ff5ac
Dennis Stücken <dstuecken@me.com>
2017-12-05 10:40:56 +0100

    elastic search document updating implemented, also implemented searching for content

--- app/listeners/ElasticSearch.php
+++ app/listeners/ElasticSearch.php
@@ -2,81 +2,143 @@
 
 namespace DS\Listeners;
 
-use DS\Application;
-use Elastica\Document;
-use Elastica\Exception\ResponseException;
-use Elastica\Index;
-use Elastica\Response;
 use Bernard\Message\PlainMessage;
+use DS\Application;
 use DS\Component\ServiceManager;
-
+use DS\Model\TableRows;
+use Elastica\Exception\NotFoundException;
+use Elastica\Exception\ResponseException;
+use Phalcon\Logger;
 
 class ElasticSearch
 {
-
+    
     /**
      * Touched a table updated content or added a new record
-     * @param  PlainMessage $message
-     * @return void or error
+     *
+     * @param PlainMessage $message
+     *
+     * @throws \Phalcon\Exception
      */
     public function touchTable(PlainMessage $message)
     {
-
-      try {
-
-        // New record or update
-        $isUpdate = $message->get('isUpdated');
-
-        // Create a table
-        $tableData = [
-          'id' => $message->get('tableId'),
-          'title' => $message->get('tableTitle'),
-          'tagline' => $message->get('tableTagline')
-        ];
-
-        // getClient
-        $elasticaClient = self::getClient();
-
-        // Document addition
-        $tableDocument = new \Elastica\Document($message->get('tableId'), $tableData);
-
-        // Add new or update Table
-        if ($isUpdate) {
-          $elasticaClient->updateDocument($tableDocument);
-        } else {
-          $elasticaClient->addDocument($tableDocument);
+        
+        try
+        {
+            echo "Received request for table \"" . $message->get('tableTitle') . "\".\n";
+            
+            $tableRows = new TableRows();
+            $rows      = $tableRows->getRowsForTable($message->get('tableId'), 0);
+            $content   = implode(
+                "\n",
+                array_map(
+                    function ($row) {
+                        $content = json_decode($row['content'], true);
+                        if ($content)
+                        {
+                            return implode(
+                                ',',
+                                array_map(
+                                    function ($ar) {
+                                        return $ar['content'];
+                                    },
+                                    $content
+                                )
+                            );
+                        }
+                        
+                        return str_repeat(',', count($content));
+                    },
+                    $rows->toArray()
+                )
+            );
+            
+            // Create a table
+            $tableData = [
+                'id' => $message->get('tableId'),
+                'title' => $message->get('tableTitle'),
+                'tagline' => $message->get('tableTagline'),
+                'content' => $content,
+            ];
+            
+            // getClient
+            $elasticaClient = self::getClient();
+            
+            // Load index
+            $elasticaIndex = $elasticaClient->getIndex('tables');
+            $elasticaType  = $elasticaIndex->getType('table');
+            
+            // Document addition
+            try
+            {
+                $tableDocument = $elasticaType->getDocument($message->get('tableId'));
+            }
+            catch (NotFoundException $e)
+            {
+                // ..
+            }
+            
+            // New record or update
+            $isUpdate = !!$tableDocument;
+            
+            if (!$isUpdate)
+            {
+                $isUpdate      = false;
+                $tableDocument = new \Elastica\Document($message->get('tableId'));
+            }
+            
+            // Document addition
+            $tableDocument->setData($tableData);
+            
+            // Add new or update Table
+            if ($isUpdate)
+            {
+                $elasticaClient->updateDocument($tableDocument);
+                echo "Updated document.\n";
+            }
+            else
+            {
+                $elasticaClient->addDocument($tableDocument);
+                echo "Created new document.\n";
+            }
+            
         }
-
-
-      } catch (ResponseException $e) {
-
-        echo "error";
-
-        $error = $e->getResponse()->getFullError();
-
-        Application::instance()->log($error['type'].':'.$error['reason'], Logger::CRITICAL);
-
-      }
-
+        catch (ResponseException $e)
+        {
+            $error = $e->getResponse()->getFullError();
+            Application::instance()->log($error['type'] . ':' . $error['reason'], Logger::CRITICAL);
+            
+        }
+        catch (\Exception $e)
+        {
+            echo var_export($e->getMessage(), true);
+            Application::instance()->log($e->getMessage(), Logger::CRITICAL);
+        }
+        
     }
-
-    // Get the ElasticSearch Service
-    public static function getClient(){
-
-      // getClient
-      $elasticaClient = ServiceManager::instance()->getDI()->get('elasticSearch');
-
-      // Load index
-      $elasticaIndex = $elasticaClient->getIndex('tables');
-
-      if (!$elasticaIndex->exists()) {
-        # Create index
-        $elasticaIndex->create();
-      }
-
-      // Get type
-      return $elasticaIndex->getType('table');
-
+    
+    /**
+     * Get the ElasticSearch Service
+     *
+     * @return \Elastica\Type
+     */
+    public static function getClient()
+    {
+        
+        // getClient
+        $elasticaClient = ServiceManager::instance()->getElasticSearch();
+        
+        // Load index
+        $elasticaIndex = $elasticaClient->getIndex('tables');
+        
+        if (!$elasticaIndex->exists())
+        {
+            # Create index
+            $elasticaIndex->create();
+        }
+        
+        // Get type
+        return $elasticaIndex->getType('table');
     }
-
+    
 }
