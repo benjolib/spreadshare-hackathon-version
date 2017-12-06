7b20771c182ffd24437f6220f7a72c80ec558b4c
Dennis Stücken <dstuecken@me.com>
2017-12-06 20:21:05 +0100

    build script updated to also update css files

--- react/build.js
+++ react/build.js
@@ -7,15 +7,31 @@ const path = require("path");
 
 // vars
 const mainLayoutFile = "../app/views/layouts/main.volt";
-const directory = "../public/js/react";
+const jsDirectory = "../public/js/react";
+const cssDirectory = "../public/css";
 
 // Removing previous build
-fs.readdir(directory, (err, files) => {
+fs.readdir(jsDirectory, (err, files) => {
   if (err) throw err;
 
   files.forEach(file => {
-    if (file !== ".gitkeep") {
-      fs.unlink(path.join(directory, file), err2 => {
+    const found = file.match(/(main\..*?\.js)$/i);
+    if (found) {
+      fs.unlink(path.join(jsDirectory, file), err2 => {
+        if (err2) throw err2;
+      });
+    }
+  });
+});
+
+// Removing css files from previous build
+fs.readdir(cssDirectory, (err, files) => {
+  if (err) throw err;
+
+  files.forEach(file => {
+    const found = file.match(/(main\..*?\.css)$/i);
+    if (found) {
+      fs.unlink(path.join(cssDirectory, file), err2 => {
         if (err2) throw err2;
       });
     }
@@ -29,9 +45,36 @@ const options = {
   dot: false
 };
 
-copy("./build/static/js", "../public/js/react", options)
+copy("./build/static/css", cssDirectory, options)
   .then(results => {
-    console.info(`Copied ${results.length} files`);
+    console.info(`Copied ${results.length} css files`);
+
+    // Replacing main bundle reference in backend's layout file
+    results.forEach(result => {
+      const found = result.dest.match(/css\/(main\..*?\.css)$/i);
+      if (found) {
+        const buildFile = found[1];
+
+        fs.readFile(mainLayoutFile, "utf8", (err, data) => {
+          if (err) {
+            return console.log(err);
+          }
+          const resultAfter = data.replace(/main\..*?\.css/g, buildFile);
+
+          fs.writeFile(mainLayoutFile, resultAfter, "utf8", err2 => {
+            if (err2) return console.log(err2);
+          });
+        });
+      }
+    });
+  })
+  .catch(error => {
+    console.error(`Copy failed: ${error}`);
+  });
+
+copy("./build/static/js", jsDirectory, options)
+  .then(results => {
+    console.info(`Copied ${results.length} js files`);
 
     // Replacing main bundle reference in backend's layout file
     results.forEach(result => {
