f432c4c9faef33e5b282e562ada38bf0988cea67
Stanley Day <stan@standayweb.com>
2018-03-20 01:04:31 +0000

    test something

--- app/controllers/TableDetailController.php
+++ app/controllers/TableDetailController.php
@@ -33,51 +33,8 @@ class TableDetailController extends BaseController
             $this->response->redirect('/');
 
             return;
-        }
-
-        // Assign table data to UI
-        $tableModel = Tables::get($tableId);
-
-        // Check if table is deleted
-        if ($tableModel->getFlags() == TableFlags::Deleted) {
-            throw new Exception('This table has been deleted!');
-        }
-
-        if ($tableModel->getFlags() == TableFlags::Archived) {
-            throw new Exception('This table has been archived! It was maybe flagged as inappropriate content.');
-        }
-
-        if ($tableModel->getFlags() == TableFlags::Unpublished) {
-            if ($tableModel->getOwnerUserId() == $this->serviceManager->getAuth()->getUserId()) {
-                // header(sprintf('Location: /table/add/confirm?tableId=%s&redirectToTable', $tableModel->getId()));
-            } else {
-                throw new Exception('You are not allowed to view this table.');
-            }
-        }
-
-        // Assign tablestats (e.g. for changelog notification badge)
-        $this->view->setVar('tableStats', (new TableStats)->get($tableId, 'tableId'));
-
-        $subClass = 'DS\\Controller\\Table\\' . ucfirst($tab);
-        if (class_exists($subClass)) {
-            $tables = $tableModel->findTablesAsArray(
-                $this->serviceManager->getAuth()->getUserId(),
-                (new TableFilter)->addTableId($tableId),
-                TableFlags::All
-            );
-
-            if (isset($tables[0])) {
-                $this->view->setVar('table', $tables[0]);
-            }
-
-            /**
-             * @var \DS\Interfaces\TableSubcontrollerInterface $subController
-             */
-            $subController = new $subClass();
-            if (is_a($subController, 'DS\Interfaces\TableSubcontrollerInterface')) {
-                $subController->initialize();
-                $subController->handle($tableModel, $this->serviceManager->getAuth()->getUserId(), $tab, $param);
-            }
+        } else {
+            header('Location: /list/' . $tableId);
         }
     }
 }
