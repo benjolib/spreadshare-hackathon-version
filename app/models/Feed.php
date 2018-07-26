f934c25613eb7a389a582b3fd747543251ee20db
AntonioHS <antonienko@gmail.com>
2018-07-26 13:57:28 +0200

    Debugging feed

--- app/models/Feed.php
+++ app/models/Feed.php
@@ -158,6 +158,7 @@ class Feed extends \DS\Model\Base
                     AND t.flags = '. TableFlags::Published.'
                 ORDER BY tr.createdAt DESC
                 LIMIT '.$limit.' OFFSET '.$offset;
+            var_dump($query);die();
             return $this->getModelsManager()
                 ->createQuery($query)->execute([
                     'userId' => $userId,
