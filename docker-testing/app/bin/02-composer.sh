6873338c5ff529849e76ba1d1224cb4ba1769838
Vladislav Klimenko <sunsingerus@gmail.com>
2018-08-15 11:01:31 +0300

    env: fix bash script error in MacOS - it is more strict than Linux

--- docker-testing/app/bin/02-composer.sh
+++ docker-testing/app/bin/02-composer.sh
@@ -8,10 +8,12 @@ set -x
 # Give composer more time to fetch packages
 export COMPOSER_PROCESS_TIMEOUT=7200
 
-if [ $PROVISION_CONTEXT != "development" ]; then
-	composer --working-dir=/application/ install --no-dev --classmap-authoritative
-else
+if [ "${PROVISION_CONTEXT}" == "development" ]; then
+    # For dev environment install all packages
     composer --working-dir=/application/ install --classmap-authoritative
+else
+    # for other environments do not install dev packages
+	composer --working-dir=/application/ install --no-dev --classmap-authoritative
 fi
 
 echo "Completed - composer dependencies (for $PROVISION_CONTEXT)"
